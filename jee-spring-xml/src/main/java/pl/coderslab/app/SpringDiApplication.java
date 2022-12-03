7365985e462582f5281cf81ecd7bd6d4ed9a24e3
bbobrek <bartosz.bobrek1993@gmail.com>
2022-12-03 15:49:23 +0100

    spring xml all zads

--- jee-spring-xml/src/main/java/pl/coderslab/app/SpringDiApplication.java
+++ jee-spring-xml/src/main/java/pl/coderslab/app/SpringDiApplication.java
@@ -1,13 +1,33 @@
 package pl.coderslab.app;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import pl.coderslab.beans.EmailService;
+import pl.coderslab.beans.HelloWorld;
+import pl.coderslab.beans.MessageSender;
+import pl.coderslab.beans.MessageService;
 
 public class SpringDiApplication {
 
     public static void main( String[] args ) {
         ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("beans.xml");
 
+        HelloWorld helloWorld = ctx.getBean("helloWorld", HelloWorld.class);
+        helloWorld.printMessage();
 
+        HelloWorld helloWorld2 = ctx.getBean("helloWorld", HelloWorld.class);
+        helloWorld2.printMessage();
+
+        HelloWorld helloWorld3 = ctx.getBean("helloWorld", HelloWorld.class);
+        helloWorld3.printMessage();
+
+        System.out.println(helloWorld.getMessage());
+
+        MessageService messageService = ctx.getBean("emailService", EmailService.class);
+        messageService.send();
+
+        MessageSender messageSender = ctx.getBean("messageSender", MessageSender.class);
+        messageSender.sendMessage();
+        messageSender.sendMessageFromProperty();
 
         ctx.close();
     }
--- jee-spring-xml/src/main/java/pl/coderslab/samples/Main.java
+++ jee-spring-xml/src/main/java/pl/coderslab/samples/Main.java
@@ -3,10 +3,10 @@ package pl.coderslab.samples;
 public class Main {
 
     public static void main(String[] args) {
-        MessageSender messageSender = new MessageSender(new EmailService(), helloWorld);
+        MessageSender messageSender = new MessageSender(new EmailService());
         messageSender.sendMessage();
 
-        MessageSender messageSender2 = new MessageSender(new SmsService(), helloWorld);
+        MessageSender messageSender2 = new MessageSender(new SmsService());
         messageSender2.sendMessage();
 
     }
