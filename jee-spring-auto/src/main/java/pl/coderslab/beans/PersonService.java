c13872d6d79392c8d937d4e52ecd57fe8e2db89a
bbobrek <bartosz.bobrek1993@gmail.com>
2022-12-03 16:16:48 +0100

    spring auto

---
+++ jee-spring-auto/src/main/java/pl/coderslab/beans/PersonService.java
@@ -0,0 +1,19 @@
+package pl.coderslab.beans;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PersonService {
+
+    private PersonRepository personRepository;
+
+    public PersonService(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    public PersonRepository getPersonRepo() {
+        return personRepository;
+    }
+
+}
