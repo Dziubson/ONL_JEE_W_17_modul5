801deeeed42649d4ab3e4882b5bab3f2542c2777
bbobrek <bartosz.bobrek1993@gmail.com>
2022-12-04 14:15:33 +0100

    spring mvc / views / zad 2

--- jee-spring-mvc/src/main/java/pl/coderslab/controller/HelloController.java
+++ jee-spring-mvc/src/main/java/pl/coderslab/controller/HelloController.java
@@ -1,16 +1,29 @@
 package pl.coderslab.controller;
 
+import org.springframework.cglib.core.Local;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.time.LocalTime;
+
 @Controller
 public class HelloController {
 
     @GetMapping("/helloView")
-    public String helloView() {
+    public String helloView(Model model) {
+        LocalTime actualTime = LocalTime.now();
+        if (actualTime.isAfter(LocalTime.of(8, 0)) &&
+                actualTime.isBefore(LocalTime.of(20, 0))) {
+            model.addAttribute("color", "black");
+            model.addAttribute("backgroundColor", "white");
+        } else {
+            model.addAttribute("color", "white");
+            model.addAttribute("backgroundColor", "black");
+        }
         return "home";
     }
 
