4cfbed67202f25ca793332fb97fe0b7f517ba664
bbobrek <bartosz.bobrek1993@gmail.com>
2022-12-04 12:02:36 +0100

    spring mvc / paramteres / zad 3- 5

--- jee-spring-mvc/src/main/java/pl/coderslab/controller/HelloController.java
+++ jee-spring-mvc/src/main/java/pl/coderslab/controller/HelloController.java
@@ -1,11 +1,19 @@
 package pl.coderslab.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
+
+    @GetMapping(value = "/hello/{firstName}/{lastName}", produces = "text/plain;charset=UTF-8")
+    @ResponseBody
+    public String hello(@PathVariable("firstName") String firstName, @PathVariable("lastName") String lastName) {
+        return "Witaj " + firstName + " " + lastName;
+    }
 
     @RequestMapping("/hello")
     @ResponseBody
