ee02ed84a9cb976764a9cf841b77f4501fe07461
bbobrek <bartosz.bobrek1993@gmail.com>
2022-12-04 13:05:03 +0100

    spring mvc / views / zad 1

--- jee-spring-mvc/src/main/java/pl/coderslab/controller/HelloController.java
+++ jee-spring-mvc/src/main/java/pl/coderslab/controller/HelloController.java
@@ -9,6 +9,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HelloController {
 
+    @GetMapping("/helloView")
+    public String helloView() {
+        return "home";
+    }
+
     @GetMapping(value = "/hello/{firstName}/{lastName}", produces = "text/plain;charset=UTF-8")
     @ResponseBody
     public String hello(@PathVariable("firstName") String firstName, @PathVariable("lastName") String lastName) {
---
+++ jee-spring-mvc/src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Bartek
+  Date: 04.12.2022
+  Time: 12:48
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+
+    <h1>Witaj w widoku</h1>
+
+</body>
+</html>
